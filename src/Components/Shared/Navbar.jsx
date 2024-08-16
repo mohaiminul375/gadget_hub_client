948ecf1262db4986f4a778def1c6e6f7b03de557
Mohaiminul375 <mohaiminul375@gmail.com>
2024-08-16 19:22:16 +0600

    add:create layout navbar

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,11 +1,39 @@
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className="bg-red-800">
-            Navbar
+    const navItems=(<>
+    <NavLink>Home</NavLink>
+    <NavLink>LogIN</NavLink>
+    <NavLink>Register</NavLink>
+    </>)
+  return (
+    <div className="navbar bg-[#4A249D] md:h-16 z-10 fixed">
+        <div className="navbar-start">
+          <h2 className="text-lg md:text-3xl text-[#55E6A5] shadow-2xl font-Matemasie">
+           Gadget Hub
+          </h2>
         </div>
-    );
+        {/* middle side content */}
+        <div className="flex-1"></div>
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 gap-4 items-center">{navItems}</ul>
+        </div>
+        {/* dropdown */}
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <GiHamburgerMenu />
+          </div>
+          <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-[#141C27] rounded-box w-52"
+          >
+            {navItems}
+          </ul>
+        </div>
+    
+    </div>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Matemasie: '"Matemasie", sans-serif',
+        Raleway: '"Raleway", sans-serif',
+      },
+    },
   },
   plugins: [daisyui],
   daisyui: { themes: ["light"] },
