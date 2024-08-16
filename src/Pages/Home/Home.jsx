98c0076fb6f44e9ad9e050097faf8ec73ce07815
Mohaiminul375 <mohaiminul375@gmail.com>
2024-08-16 20:04:44 +0600

    load data from server

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,25 @@
-
+import { useEffect } from "react";
 
 const Home = () => {
-    return (
-        <div className="pt-20">
-            Home
+  // load data from server
+  useEffect(() => {
+    fetch(`http://localhost:3000/products`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+  return (
+    <section className="pt-20">
+      Home
+      {/* products */}
+      <section>
+        <div>
+          <h2 className="text-center text-4xl font-extrabold">All Products</h2>
         </div>
-    );
+      </section>
+      {/* All Products */}
+      <section></section>
+    </section>
+  );
 };
 
 export default Home;
