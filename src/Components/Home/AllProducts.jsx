c8b2e5ea6ea50c7ddd8a74839c89e485483048b4
Mohaiminul375 <mohaiminul375@gmail.com>
2024-08-17 15:30:53 +0600

    update: pagination working on all product

--- src/Components/Home/AllProducts.jsx
+++ src/Components/Home/AllProducts.jsx
@@ -9,8 +9,10 @@ const AllProducts = () => {
   const [date, setDate] = useState("");
   // price sorting
   const [price, setPrice] = useState("");
-  console.log(date);
-  console.log(price);
+// pagination
+const [itemPerPg,setItemPerPg]=useState(10);
+const [count, setCount] = useState(0);
+const [currentPg, setCurrentPg] = useState(1);
   // search product name
   const searchProduct = (e) => {
     e.preventDefault();
@@ -18,13 +20,33 @@ const AllProducts = () => {
     const product_name = e.target.name.value;
     setSearch(product_name);
   };
-
+  // pagination
+  const handlePaginationBtn=(currentBtn)=>{
+    setCurrentPg(currentBtn)
+  }
+// page number
+const numberOfPages = Math.ceil(count / itemPerPg);
+const pages = [...Array(numberOfPages).keys()].map((e) => e + 1);
   // load data from server
   useEffect(() => {
-    fetch(`http://localhost:3000/products?name=${search}&sort_date=${date}&sort_price=${price}`)
+    fetch(
+      `http://localhost:3000/products?page=${currentPg}&size=${itemPerPg}&name=${search}&sort_date=${date}&sort_price=${price}`
+    )
       .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, [search,date,price]);
+      .then((data) => {
+        setProducts(data)
+      // setCount(data.length)
+      });
+  }, [search, date, price,currentPg,itemPerPg]);
+
+ //load data for manage pagination
+ useEffect(()=>{
+  fetch(`http://localhost:3000/products?name=${search}&sort_date=${date}&sort_price=${price}`)
+  .then(res=>res.json())
+  .then(data=>setCount(data.length))
+ },[search,date,price])
+
+
   return (
     <section className="mt-10">
       {/* search and filtering */}
@@ -54,8 +76,8 @@ const AllProducts = () => {
           <option disabled selected>
             Price
           </option>
-          <option value='low_to_high'>Low to High</option>
-          <option value='high_to_low'>Hight to Low</option>
+          <option value="low_to_high">Low to High</option>
+          <option value="high_to_low">Hight to Low</option>
         </select>
         {/* date */}
         <select
@@ -66,8 +88,8 @@ const AllProducts = () => {
           <option disabled selected>
             Sorting Date
           </option>
-          <option value='newest_first'>Newest First</option>
-          <option value='older_first'>Older First</option>
+          <option value="newest_first">Newest First</option>
+          <option value="older_first">Older First</option>
         </select>
       </section>
 
@@ -80,6 +102,73 @@ const AllProducts = () => {
           {products.map((product) => (
             <ProductCard key={product._id} product={product}></ProductCard>
           ))}
+        </div>
+      </section>
+      {/* pagination */}
+      <section>
+        <div className="flex justify-center mt-12">
+          {/* Previous*/}
+          <button
+            disabled={currentPg === 1}
+            onClick={() => handlePaginationBtn(currentPg - 1)}
+            className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-[#247CFF] hover:text-white"
+          >
+            <div className="flex items-center -mx-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M7 16l-4-4m0 0l4-4m-4 4h18"
+                />
+              </svg>
+
+              <span className="mx-1">previous</span>
+            </div>
+          </button>
+          {/*pagination number */}
+          {pages.map((btnNum) => (
+            <button
+              onClick={() => handlePaginationBtn(btnNum)}
+              key={btnNum}
+              className={`hidden ${
+                currentPg === btnNum ? "bg-[#247CFF] text-white" : ""
+              } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-[#247CFF]  hover:text-white`}
+            >
+              {btnNum}
+            </button>
+          ))}
+          {/* Next */}
+          <button
+            disabled={currentPg === numberOfPages}
+            onClick={() => handlePaginationBtn(currentPg + 1)}
+            className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-[#247CFF] disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+          >
+            <div className="flex items-center -mx-1">
+              <span className="mx-1">Next</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mx-1 rtl:-scale-x-100"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </div>
+          </button>
         </div>
       </section>
     </section>
